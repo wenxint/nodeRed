da4c9383c1c0be11e507fb9b126e462fed5304db
martintao <martintao@tencent.com>
2025-04-29 14:38:47 +0800

    feat: 添加多个Proto文件及更新现有文件
    新增多个Proto文件以支持新功能，包括actpb、zonepb等模块的请求和响应定义。同时，对现有Proto文件进行了更新，增加了新的字段和注释，确保与最新需求保持一致，提升了代码的可读性和维护性。

--- app.js
+++ app.js
@@ -80,24 +80,36 @@ redApiRoutes.forEach((route) => {
 });
 
 /**
- * @description 在应用启动时从 SVN 同步 Proto 文件目录
- * @see http://tc-svn.tencent.com/KungfuTeam/Red_proj/trunk/RedApp/Content/Script/Red/Net
+ * @description 在应用启动时从 SVN 导出 Proto 文件夹内容到本地Proto目录
+ * @see http://tc-svn.tencent.com/KungfuTeam/Red_proj/trunk/RedApp/Content/Script/Red/Net/Proto
  */
 const { execSync } = require("child_process");
-const protoSVNRepo = 'http://tc-svn.tencent.com/KungfuTeam/Red_proj/trunk/RedApp/Content/Script/Red/Net';
+const protoSVNRepo = 'http://tc-svn.tencent.com/KungfuTeam/Red_proj/trunk/RedApp/Content/Script/Red/Net/Proto';
 const protoDir = path.join(__dirname, "Proto");
+
+// 检查svn命令是否可用
+let svnAvailable = true;
 try {
-  // 如果目录不存在或不是工作副本，则执行 checkout；否则执行 update
-  const svnMeta = path.join(protoDir, '.svn');
-  if (!fs.existsSync(protoDir) || !fs.existsSync(svnMeta)) {
-    console.info('SVN 初次检出 Proto 文件...');
-    execSync(`svn checkout "${protoSVNRepo}" "${protoDir}" --username a1_red --password A1Red@dev --no-auth-cache --non-interactive --trust-server-cert`, { stdio: 'inherit' });
-  } else {
-    console.info('SVN 更新 Proto 文件...');
-    execSync(`svn update "${protoDir}" --username a1_red --password A1Red@dev --no-auth-cache --non-interactive --trust-server-cert`, { stdio: 'inherit' });
+  execSync("svn --version", { stdio: "ignore" });
+} catch (err) {
+  svnAvailable = false;
+}
+if (!svnAvailable) {
+  console.warn("未检测到svn命令，跳过Proto文件夹导出");
+} else {
+  try {
+    // 确保本地Proto目录存在
+    if (!fs.existsSync(protoDir)) {
+      fs.mkdirSync(protoDir, { recursive: true });
+    }
+    console.info("SVN导出Proto文件...");
+    execSync(
+      `svn export --force "${protoSVNRepo}" "${protoDir}" --username a1_red --password A1Red@dev --no-auth-cache --non-interactive --trust-server-cert`,
+      { stdio: "inherit" }
+    );
+  } catch (error) {
+    console.error("Proto文件导出失败:", error);
   }
-} catch (error) {
-  console.error('Proto 文件 SVN 同步失败:', error);
 }
 
 app.use(function (req, res, next) {
